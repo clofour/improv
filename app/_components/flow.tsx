e6799dad5eb3acaf2f770b02c68cb62b82de89b2
clofour <208706398+clofour@users.noreply.github.com>
2026-07-04 22:35:18 +0200

    landing: implement section wrapper in flow

--- app/_components/flow.tsx
+++ app/_components/flow.tsx
@@ -1,3 +1,16 @@
+import Terminal from "@/components/terminal";
+import Section from "./section";
+
 export default function Flow() {
-	return <div>Hello!</div>;
+	return (
+		<Section
+			id="how"
+			title="how it works"
+			description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id."
+		>
+			<Terminal title="architecture" className="w-full h-full">
+				Hello!
+			</Terminal>
+		</Section>
+	);
 }
