f1de6c5055534f878fbc494fc44bbda699abb696
clofour <208706398+clofour@users.noreply.github.com>
2026-07-04 15:49:56 +0200

    landing: implement navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import FAQ from "./_components/faq";
 import Flow from "./_components/flow";
 import Footer from "./_components/footer";
 import Hero from "./_components/hero";
+import Navbar from "./_components/navbar";
 import Prizes from "./_components/prizes";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 p-5 items-center justify-center">
-      <Hero />
-      <Flow />
-      <Prizes />
-      <FAQ />
-      <Footer />
+    <div>
+      <Navbar />
+      <div className="flex flex-col flex-1 p-5 items-center justify-center">
+        <Hero />
+        <Flow />
+        <Prizes />
+        <FAQ />
+        <Footer />
+      </div>
     </div>
   );
 }
