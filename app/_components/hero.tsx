d91e2e0affaabe788f28a9628bef750cf2f9c67d
clofour <208706398+clofour@users.noreply.github.com>
2026-07-04 20:45:46 +0200

    landing: flesh out hero further

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -1,5 +1,17 @@
+import Terminal from "@/components/terminal";
+
 export default function Hero() {
-  return (
-    <div>Hello!</div>
-  );
+	return (
+		<div className="grid grid-cols-1 sm:grid-cols-2 w-full h-full gap-10">
+			<div className="w-full h-full">
+				<h1>improvise your own infrastructure</h1>
+				<p>a lot of text</p>
+			</div>
+			<div className="w-full h-full">
+				<Terminal className="w-full h-full" title="HELLO">
+					<p>IDK?</p>
+				</Terminal>
+			</div>
+		</div>
+	);
 }
