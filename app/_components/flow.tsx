234633d0dba8527f2a67e0a9760966e288d17d5d
clofour <208706398+clofour@users.noreply.github.com>
2026-07-05 00:59:49 +0200

    landing: start implementation of flow

--- app/_components/flow.tsx
+++ app/_components/flow.tsx
@@ -2,6 +2,19 @@ import Terminal from "@/components/terminal";
 import Section from "./section";
 
 export default function Flow() {
+	const columns = [
+		["Come up with an awesome idea!!"],
+		["Use existing provisioning tools", "Create your own provisioning tools"],
+		["Ship your project"],
+		["Receive Uptime"],
+		["Buy prizes from the shop"],
+	];
+
+	function generateCurve(x1: number, y1: number, x2: number, y2: number) {
+		const offset = (x2 - x1) * 0.5;
+		return `M ${x1} ${y1} C ${x1 + offset} ${y1}, ${x2 - offset} ${y2}, ${x2} ${y2}`;
+	}
+
 	return (
 		<Section
 			id="how"
@@ -9,7 +22,18 @@ export default function Flow() {
 			description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae pellentesque sem placerat in id."
 		>
 			<Terminal title="architecture" className="w-full h-full">
-				Hello!
+				<div className="w-full h-full relative px-5 py-10">
+					<svg className="w-full h-full absolute inset-0 pointer-events-none"></svg>
+					<div className="w-full h-full flex flex-row gap-30">
+						{columns.map((column) => (
+							<div className="w-full h-full flex flex-col justify-between gap-10">
+								{column.map((step) => (
+									<div className="mx-auto">{step}</div>
+								))}
+							</div>
+						))}
+					</div>
+				</div>
 			</Terminal>
 		</Section>
 	);
