8323de946345687fe426b2d8c48d84121608078e
clofour <208706398+clofour@users.noreply.github.com>
2026-07-04 14:20:19 +0200

    components: implement external link

---
+++ components/external-link.tsx
@@ -0,0 +1,12 @@
+
+type ExternalLinksProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+    href: string;
+}
+
+export default function ExternalLink({href, children, ...props}: ExternalLinksProps) {
+    return (
+        <a href={href} target="_blank" rel="noopener noreferrer" {...props}>
+            {children}
+        </a>
+    )
+}
