c8267d181b447cd7176f2b605b84d254be615572
clofour <208706398+clofour@users.noreply.github.com>
2026-07-04 22:35:06 +0200

    landing: implement section wrapper

---
+++ app/_components/section.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+
+interface SectionProps {
+	id: string;
+	title: string;
+	description: string;
+	children: ReactNode;
+}
+
+export default function Section({
+	id,
+	title,
+	description,
+	children,
+}: SectionProps) {
+	return (
+		<div id={id} className="flex flex-col w-full h-full px-6 py-16 gap-6">
+			<div className="flex flex-col gap-3">
+				<div className="text-3xl sm:text-4xl font-heading font-bold">
+					{title}
+				</div>
+				<div className="text-muted-foreground text-base font-mono">
+					{description}
+				</div>
+			</div>
+
+			<div>{children}</div>
+		</div>
+	);
+}
