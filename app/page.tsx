4b66fcf6146edf4d81548f656d9c53b38df4c087
clofour <208706398+clofour@users.noreply.github.com>
2026-07-06 14:28:02 +0200

    landing: fix various layout issues

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ import Prizes from "./_components/prizes";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="min-w-screen min-h-screen">
 			<Navbar />
-			<div className="flex flex-col flex-1 items-center justify-center">
+			<main className="flex flex-col flex-1 items-center justify-center">
 				<Hero />
 				<Flow />
 				<Prizes />
 				<FAQ />
 				<Footer />
-			</div>
+			</main>
 		</div>
 	);
 }
