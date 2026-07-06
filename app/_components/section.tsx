a9135c38ab43c628ff8f1e21476ca677439463f6
clofour <208706398+clofour@users.noreply.github.com>
2026-07-06 18:10:51 +0200

    landing: reduce spacing between sections (via padding)

--- app/_components/section.tsx
+++ app/_components/section.tsx
@@ -14,7 +14,7 @@ export default function Section({
 	children,
 }: SectionProps) {
 	return (
-		<div id={id} className="flex flex-col w-full h-full px-6 py-16 gap-6">
+		<div id={id} className="flex flex-col w-full h-full px-6 py-14 gap-6">
 			<div className="flex flex-col gap-3">
 				<div className="text-3xl sm:text-4xl font-heading font-bold">
 					{title}
