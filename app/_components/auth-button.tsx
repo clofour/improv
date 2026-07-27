23cdbe0773f8175c8a6fa7516febade3890729e8
clofour <208706398+clofour@users.noreply.github.com>
2026-07-27 18:06:58 +0300

    global,landing,db: progress towards working auth

--- app/_components/auth-button.tsx
+++ app/_components/auth-button.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Button, buttonVariants } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 import { VariantProps } from "class-variance-authority";
 
 type ButtonVariants = VariantProps<typeof buttonVariants>;
@@ -9,8 +12,15 @@ interface AuthButtonProps {
 }
 
 export default function AuthButton({ variant, size }: AuthButtonProps) {
+	async function onClick() {
+		const { data, error } = await authClient.signIn.oauth2({
+			providerId: "hackclub",
+			callbackURL: "/app",
+		});
+	}
+
 	return (
-		<Button variant={variant} size={size}>
+		<Button variant={variant} size={size} onClick={onClick}>
 			Sign In
 		</Button>
 	);
