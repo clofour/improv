08cc08035b78df08223bcf775ccb2012724eb627
clofour <208706398+clofour@users.noreply.github.com>
2026-07-29 23:51:36 +0300

    shop: add key to shop item map

--- app/app/shop/page.tsx
+++ app/app/shop/page.tsx
@@ -6,7 +6,7 @@ export default function Shop() {
 		<div className="min-w-screen min-h-screen p-6">
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 				{data.map((item) => (
-					<ShopItem price={400} discountPrice={500} {...item} />
+					<ShopItem key={item.id} price={400} discountPrice={500} {...item} />
 				))}
 			</div>
 		</div>
