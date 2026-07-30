043ac739289abbbad59ffde9cfe92c79b548d459
clofour <208706398+clofour@users.noreply.github.com>
2026-07-31 00:05:52 +0300

    app: remove dead code

--- app/app/_components/background.tsx
+++ app/app/_components/background.tsx
@@ -4,45 +4,12 @@ import { useDesktop, Vector2D } from "./desktop";
 export default function Background() {
 	const selectFiles = useDesktop((state) => state.selectFiles);
 
-	// const selectOffset = useRef<Vector2D | null>(null);
-
 	function onClick() {
 		selectFiles([]);
 	}
 
-	// const onPointerDown = (e: React.PointerEvent) => {
-	// 	if (e.target != e.currentTarget) return;
-
-	// 	selectOffset.current = {
-	// 		x: e.clientX - file.position.x,
-	// 		y: e.clientY - file.position.y,
-	// 	};
-
-	// 	selectFiles([]);
-	// };
-	// const onPointerMove = (e: React.PointerEvent) => {
-	// 	if (!selectOffset.current) return;
-
-	// 	const clampX = clamp(e.clientX, 0, window.innerWidth);
-	// 	const clampY = clamp(e.clientY, 0, window.innerHeight);
-
-	// 	moveFile(id, {
-	// 		x: clampX - selectOffset.current.x,
-	// 		y: clampY - selectOffset.current.y,
-	// 	});
-	// };
-	// const onPointerUp = (e: React.PointerEvent) => {
-	// 	selectOffset.current = null;
-	// };
-
 	return (
-		<div
-			className="absolute inset-0"
-			onClick={onClick}
-			onPointerDown={onPointerDown}
-			onPointerMove={onPointerMove}
-			onPointerUp={onPointerUp}
-		>
+		<div className="absolute inset-0" onClick={onClick}>
 			<div
 				className="absolute bg-blue-600 border border-blue-600"
 				style={{
