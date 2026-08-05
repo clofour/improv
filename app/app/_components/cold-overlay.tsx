d971f6a2f12a72ab74aced557e22041157469723
clofour <208706398+clofour@users.noreply.github.com>
2026-08-05 11:58:37 +0300

    app,public: add a crystal overlay

--- app/app/_components/cold-overlay.tsx
+++ app/app/_components/cold-overlay.tsx
@@ -1,8 +1,9 @@
 export default function ColdOverlay() {
 	return (
-		<div className="opacity-25">
-			<div className="absolute inset-0 pointer-events-none bg-[url('/fx/ice.jpg')] opacity-12.5 mix-blend-screen" />
-			<div className="pointer-events-none absolute inset-0 z-20 bg-[radial-gradient(ellipse_at_8%_5%,rgba(235,245,250,0.38)_0%,rgba(220,235,240,0.18)_18%,transparent_42%),radial-gradient(ellipse_at_92%_8%,rgba(235,245,250,0.28)_0%,rgba(220,235,240,0.12)_20%,transparent_45%),radial-gradient(ellipse_at_5%_88%,rgba(235,245,250,0.18)_0%,transparent_38%),radial-gradient(ellipse_at_95%_92%,rgba(235,245,250,0.32)_0%,rgba(220,235,240,0.12)_20%,transparent_40%)]" />
+		<div className="pointer-events-none opacity-25">
+			<div className="absolute inset-0 bg-[url('/fx/crystals.jpg')] [mask-image:radial-gradient(ellipse_65%_60%_at_50%_50%,transparent_60%,black_100%)] opacity-20 mix-blend-screen" />
+			<div className="absolute inset-0 bg-[url('/fx/ice.jpg')] opacity-12.5 mix-blend-screen" />
+			<div className="absolute inset-0 bg-[radial-gradient(ellipse_at_8%_5%,rgba(235,245,250,0.38)_0%,rgba(220,235,240,0.18)_18%,transparent_42%),radial-gradient(ellipse_at_92%_8%,rgba(235,245,250,0.28)_0%,rgba(220,235,240,0.12)_20%,transparent_45%),radial-gradient(ellipse_at_5%_88%,rgba(235,245,250,0.18)_0%,transparent_38%),radial-gradient(ellipse_at_95%_92%,rgba(235,245,250,0.32)_0%,rgba(220,235,240,0.12)_20%,transparent_40%)]" />
 		</div>
 	);
 }
