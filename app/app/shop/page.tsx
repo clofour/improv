9ef492ab445e9528cb0a700e4f7f9de9356c396a
clofour <208706398+clofour@users.noreply.github.com>
2026-08-05 23:38:18 +0300

    shop: fix sizing

--- app/app/shop/page.tsx
+++ app/app/shop/page.tsx
@@ -3,7 +3,7 @@ import ShopItem from "./_components/shop-item";
 
 export default function Shop() {
 	return (
-		<div className="min-w-screen min-h-screen p-6">
+		<div className="w-full h-full p-6">
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 				{data.map((item) => (
 					<ShopItem key={item.id} price={400} discountPrice={500} {...item} />
