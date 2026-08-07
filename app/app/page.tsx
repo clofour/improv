300408bc865bd1539d446b574312664cd7481055
clofour <208706398+clofour@users.noreply.github.com>
2026-08-07 13:45:43 +0300

    app: fix outdated reference to effectmode

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,9 @@
-import { ColdEffectClient, ColdEffectMode } from "@/components/cold-effect";
+import { ColdEffectClient } from "@/components/cold-effect";
 import Bezel from "./_components/bezel";
 import ColdOverlay from "./_components/cold-overlay";
 import CRTOverlay from "./_components/crt-overlay";
 import Screen from "./_components/screen";
+import { EffectMode } from "@/components/effect-mode";
 
 export default function App() {
 	return (
@@ -13,7 +14,7 @@ export default function App() {
 				<ColdOverlay />
 			</Bezel>
 			<div className="fixed -inset-8 pointer-events-none bg-[url('/fx/snow-frame.png')] bg-cover bg-center" />
-			<ColdEffectClient mode={ColdEffectMode.Foreground} />
+			<ColdEffectClient mode={EffectMode.Foreground} />
 		</div>
 	);
 }
