6fafc8cb02db555ce45c5f68b424e2be7678b9af
clofour <208706398+clofour@users.noreply.github.com>
2026-08-10 17:11:47 +0300

    users: await authorization check

--- app/internal/users/page.tsx
+++ app/internal/users/page.tsx
@@ -5,9 +5,11 @@ import SearchBar from "./_components/search-bar";
 export default async function Users() {
 	const session = await getSession();
 	if (!session.ok) return null;
-	const authorization = checkPermissions(session.data.user.id, {
+	const authorization = await checkPermissions(session.data.user.id, {
 		user: ["list"],
 	});
+
+	// TODO: Authorization is not checked
 
 	// const result = await listUsers();
 	// if (!result.ok) return null;
