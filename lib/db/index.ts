a7b95627e935740284118ddfe7a6f156e724e8d5
clofour <208706398+clofour@users.noreply.github.com>
2026-08-10 15:43:18 +0300

    db: fix db conn in development environments

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,8 +1,12 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle as drizzlePG } from "drizzle-orm/node-postgres";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-serverless";
 import ws from "ws";
 
-export const db = drizzle({
-	connection: process.env.DATABASE_URL!,
-	ws: ws,
-});
+export const db =
+	process.env.NODE_ENV === "development"
+		? drizzlePG(process.env.DATABASE_URL!)
+		: drizzleNeon({
+				connection: process.env.DATABASE_URL!,
+				ws: ws,
+			});
