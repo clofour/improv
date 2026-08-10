127b1a8a0902edd03f66aa609467b18cb22f0a6c
clofour <208706398+clofour@users.noreply.github.com>
2026-08-11 01:19:47 +0300

    landing: use lowercase consistently

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
 							size="cta"
 							variant="outline"
 						>
-							See How It Works
+							see how it works
 						</LinkButton>
 					</div>
 				</div>
