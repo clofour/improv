4ae642d63c895e82d35ba34130e5f7bd407c36ed
clofour <208706398+clofour@users.noreply.github.com>
2026-08-22 13:51:15 +0300

    users,utils: apply minor improvements

--- app/internal/users/[id]/page.tsx
+++ app/internal/users/[id]/page.tsx
@@ -5,6 +5,7 @@ import { checkPermissions, getSession } from "@/lib/auth/utils";
 import { getUser, type User } from "@/lib/users/service";
 import { ArrowLeftIcon } from "@phosphor-icons/react";
 import BackButton from "../../_components/back-button";
+import displayBool from "@/lib/utils/display";
 
 interface UserProps {
 	params: Promise<{ id: string }>;
@@ -33,9 +34,9 @@ export default async function User({ params: paramsPromise }: UserProps) {
 			<div>
 				<div>Name: {user.name}</div>
 				<div>Email: {user.email}</div>
-				<div>Roles: {user.name}</div>
+				<div>Roles: {user.role}</div>
 				<div>Balance: {user.uptimeBalance}</div>
-				<div>Banned: {user.banned === true ? "yes" : "no"}</div>
+				<div>Banned: {displayBool(user.banned ?? false)}</div>
 			</div>
 		</div>
 	);
---
+++ lib/utils/display.ts
@@ -0,0 +1,3 @@
+export default function displayBool(bool: boolean) {
+	return bool ? "yes" : "no";
+}
